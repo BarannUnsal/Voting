c448c6d35f758ff1a7f0f6b4f8607f8cce287e83
Baran Unsal <barannunsal@gmail.com>
2022-07-13 19:48:04 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,191 @@
+﻿using Voting;
+try
+{
+    Console.WriteLine("\nLütfen kullanıcı adınızı giriniz: ");
+    string user = Console.ReadLine().TrimEnd().TrimStart();
+
+    bool log = Register.Login(user);
+    if (!log)
+    {
+        Console.WriteLine("\nKaydınız bulunamadı. Kaydınızı gerçekleştiriyoruz..");
+        Register reg = new Register(user);
+    }
+    Console.WriteLine("\nHoşgeldin {0}", user);
+start:
+    Console.WriteLine("\nLütfen Yapmak İstediğiniz işlemi seçin:\n1 - Oy Vermek\n2 - Oyları Görmek");
+    int firstRes = Convert.ToInt32(Console.ReadLine());
+    if (firstRes <= 2)
+    {
+        switch (firstRes)
+        {
+            case 1:
+                Console.WriteLine("\nLütfen oy vereceğiniz kategoriyi seçiniz," +
+        "\nSpor için 1," +
+        "\nTeknoloji için 2," +
+        "\nSinema için 3," +
+        "\nKitap için 4");
+                int select = Convert.ToInt32(Console.ReadLine());
+                Voting_ vot = new Voting_(select);
+                if (select <= 4)
+                {
+                    switch (select)
+                    {
+                        case 1:
+                            Console.WriteLine($"Spor kategorisi için oy verdiniz");
+                            Console.WriteLine("Oyunuz başarılı bir şekilde verilmiştir.\nYapmak istediğiniz işlemi seçiniz.\n1 - Yeniden oy vermek\n2 - Oyları Görmek\n3 - Çıkış");
+                            int res = Convert.ToInt32(Console.ReadLine());
+                            if (res <= 3)
+                            {
+                                switch (res)
+                                {
+                                    case 1:
+                                        goto start;
+                                    case 2:
+                                        Console.WriteLine("\nOylar:");
+                                        Console.WriteLine(Voting_.getResult());
+                                        Console.WriteLine("\nMenüye dönmek için 1\nÇıkış yapmak için 2");
+                                        int res9 = Convert.ToInt32(Console.ReadLine());
+                                        if (res9 <= 2)
+                                        {
+                                            switch (res9)
+                                            {
+                                                case 1:
+                                                    goto start;
+                                                case 2:
+                                                    Console.WriteLine("Görüşmek üzere...");
+                                                    break;
+                                            }
+                                        }
+                                        break;
+                                    case 3:
+                                        Console.WriteLine("Görüşmek üzere...");
+                                        break;
+                                }
+                            }
+                            break;
+                        case 2:
+                            Console.WriteLine($"Teknoloji kategorisi için oy verdiniz");
+                            Console.WriteLine("Oyunuz başarılı bir şekilde verilmiştir.\nYapmak istediğiniz işlemi seçiniz.\n1 - Yeniden oy vermek\n2 - Oyları Görmek\n3 - Çıkış");
+                            int res1 = Convert.ToInt32(Console.ReadLine());
+                            if (res1 <= 3)
+                            {
+                                switch (res1)
+                                {
+                                    case 1:
+                                        goto start;
+                                    case 2:
+                                        Console.WriteLine("\nOylar:");
+                                        Console.WriteLine(Voting_.getResult());
+                                        Console.WriteLine("\nMenüye dönmek için 1\nÇıkış yapmak için 2");
+                                        int res8 = Convert.ToInt32(Console.ReadLine());
+                                        if (res8 <= 2)
+                                        {
+                                            switch (res8)
+                                            {
+                                                case 1:
+                                                    goto start;
+                                                case 2:
+                                                    Console.WriteLine("Görüşmek üzere");
+                                                    break;
+                                            }
+                                        }
+                                        break;
+                                    case 3:
+                                        Console.WriteLine("Görüşmek üzere...");
+                                        break;
+                                }
+                            }
+                            break;
+                        case 3:
+                            Console.WriteLine($"Sinema kategorisi için oy verdiniz");
+                            Console.WriteLine("Oyunuz başarılı bir şekilde verilmiştir.\nYapmak istediğiniz işlemi seçiniz.\n1 - Yeniden oy vermek\n2 - Oyları Görmek\n3 - Çıkış");
+                            int res2 = Convert.ToInt32(Console.ReadLine());
+                            if (res2 <= 3)
+                            {
+                                switch (res2)
+                                {
+                                    case 1:
+                                        goto start;
+                                    case 2:
+                                        Console.WriteLine("\nOylar:");
+                                        Console.WriteLine(Voting_.getResult());
+                                        Console.WriteLine("\nMenüye dönmek için 1\nÇıkış yapmak için 2");
+                                        int res7 = Convert.ToInt32(Console.ReadLine());
+                                        if (res7 <= 2)
+                                        {
+                                            switch (res7)
+                                            {
+                                                case 1:
+                                                    goto start;
+                                                case 2:
+                                                    Console.WriteLine("Görüşmek üzere");
+                                                    break;
+                                            }
+                                        }
+                                        break;
+                                    case 3:
+                                        Console.WriteLine("Görüşmek üzere...");
+                                        break;
+                                }
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine($"Kitap kategorisi için oy verdiniz");
+                            Console.WriteLine("Oyunuz başarılı bir şekilde verilmiştir.\nYapmak istediğiniz işlemi seçiniz.\n1 - Yeniden oy vermek\n2 - Oyları Görmek\n3 - Çıkış");
+                            int res3 = Convert.ToInt32(Console.ReadLine());
+                            if (res3 <= 3)
+                            {
+                                switch (res3)
+                                {
+                                    case 1:
+                                        goto start;
+                                    case 2:
+                                        Console.WriteLine("\nOylar:");
+                                        Console.WriteLine(Voting_.getResult());
+                                        Console.WriteLine("\nMenüye dönmek için 1\nÇıkış yapmak için 2");
+                                        int res7 = Convert.ToInt32(Console.ReadLine());
+                                        if (res7 <= 2)
+                                        {
+                                            switch (res7)
+                                            {
+                                                case 1:
+                                                    goto start;
+                                                case 2:
+                                                    Console.WriteLine("Görüşmek üzere");
+                                                    break;
+                                            }
+                                        }
+                                        break;
+                                    case 3:
+                                        Console.WriteLine("Görüşmek üzere...");
+                                        break;
+                                }
+                            }
+                            break;
+                    }
+                }
+                break;
+            case 2:
+                Console.WriteLine("\nOylar:");
+                Console.WriteLine(Voting_.getResult());
+                Console.WriteLine("\nMenüye dönmek için 1\nÇıkış Yapmak için 2");
+                int lastRes = Convert.ToInt32(Console.ReadLine());
+                if (lastRes <= 2)
+                {
+                    switch (lastRes)
+                    {
+                        case 1:
+                            goto start;
+                        case 2:
+                            Console.WriteLine("Görüşmek üzere...");
+                            break;
+                    }
+                }
+                break;
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("Hata!!", e.Message);
+}
---
+++ Register.cs
@@ -0,0 +1,69 @@
+﻿namespace Voting
+{
+    public class Register
+    {
+        static string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\votingLog.txt";
+
+        public Register(string username)
+        {
+            if (!File.Exists(path))
+                File.Create(path).Close();
+
+            using (StreamWriter sr = new StreamWriter(path, append: true))
+            {
+                sr.WriteLine("\n{0}", username);
+            }
+
+        }
+        static public bool Login(string username)
+        {
+            if (!File.Exists(path))
+                File.Create(path).Close();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                    if (sr.ReadLine().Contains(username))
+                        return true;
+                return false;
+            }
+        }
+    }
+    class Voting_
+    {
+        static string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\votingLog1.txt";
+        public Voting_(int vote)
+        {
+            if (!File.Exists(path))
+                File.Create(path).Close();
+            using (StreamWriter sw = new StreamWriter(path, append: true))
+            {
+                sw.WriteLine("\n{0}", vote);
+            }
+        }
+        public static string getResult()
+        {
+            double spor = 0, kitap = 0, sinema = 0, teknoloji = 0;
+            string res = "";
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                    switch (sr.ReadLine().TrimEnd().TrimStart())
+                    {
+                        case "1": spor++; break;
+                        case "2": teknoloji++; break;
+                        case "3": sinema++; break;
+                        case "4": kitap++; break;
+
+                        default: break;
+                    }
+                res = String.Format($"\nSonuçlar;" +
+                $"\nSpor -> %{spor / (spor + kitap + sinema + teknoloji) * 100}" +
+                $"\nTeknoloji -> %{teknoloji / (spor + kitap + sinema + teknoloji) * 100}" +
+                $"\nSinema -> %{sinema / (spor + kitap + sinema + teknoloji) * 100}" +
+                $"\nKitap -> %{kitap / (spor + kitap + sinema + teknoloji) * 100}");
+            }
+            return res;
+        }
+    }
+}
